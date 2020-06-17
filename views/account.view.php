f04a5365403bde1e6fe2b0b8802134d37361eab9
JonathanCrz1 <Jonathan_PachecoCruz@hotmail.com>
2020-06-16 20:30:50 -0500

    v7

--- views/account.view.php
+++ views/account.view.php
@@ -1,3 +1,16 @@
+ <?php 
+require 'admin/db.php';
+
+	$nombre = $_POST["nombre"];
+	$curp = $_POST["curp"];
+	$rfc = $_POST["rfc"];
+	$perfil = $_POST["perfil"];
+
+		$sql = "INSERT INTO docentes (ID_MAESTRO, NOMBRE, CURP, RFC, PERFIL_PROFESIONAL, ID_Areas) VALUES ('$nombre', '$curp', '$rfc', '$perfil', '1')";
+			$stmt = $conn->prepare($sql);
+
+ ?>
+
  <!DOCTYPE html>
  <html lang="en">
  <head>
