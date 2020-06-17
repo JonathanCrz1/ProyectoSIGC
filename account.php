839b39cc57ac6ebb0269b0f829ab2e9a5b969b27
JonathanCrz1 <Jonathan_PachecoCruz@hotmail.com>
2020-06-16 21:53:34 -0500

    v5

--- account.php
+++ account.php
@@ -1,13 +1,25 @@
  <?php 
-require 'views/account.view.php';
-require 'admin/db.php';
+ 	require 'views/account.view.php';
+	include("admin/db.php");
 
-	$nombre = $_POST["nombre"];
-	$curp = $_POST["curp"];
-	$rfc = $_POST["rfc"];
-	$perfil = $_POST["perfil"];
+	if (isset($_POST['nombre']) && !empty($_POST['nombre']) && 
+		isset($_POST['curp']) && !empty($_POST['curp']) &&
+	 	isset($_POST['rfc']) && !empty($_POST['rfc']) && 
+	 	isset($_POST['perfil']) && !empty($_POST['perfil'])){
 
-		$sql = "INSERT INTO docentes (ID_MAESTRO, NOMBRE, CURP, RFC, PERFIL_PROFESIONAL, ID_Areas) VALUES ('$nombre', '$curp', '$rfc', '$perfil', '1')";
-			$stmt = $conn->prepare($sql);
 
+
+
+		$cox = mysqli_connect($server, $username, $password, $database);
+	mysqli_query($cox,("INSERT INTO docentes (NOMBRE, CURP, RFC, PERFIL_PROFESIONAL, ID_Areas) VALUES('$_POST[nombre]','$_POST[curp]','$_POST[rfc]','$_POST[perfil]', '1')"));
+
+	echo "datos actualizados";
+	}else{
+		echo "error al actualizar";
+	}
  ?>
+
+
+
+
+
